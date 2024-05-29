dd071d68469de3382b16740674396d2e4e5bb8c8
jspnguyen <theantisine@gmail.com>
2024-05-29 14:17:58 -0700

    IMplemented add and remove

--- keyword_adder.py
+++ keyword_adder.py
@@ -4,9 +4,6 @@ from discord import app_commands
 with open('data/config.json', 'r') as f:
     config = json.load(f)
 
-with open('data/keywords.json', 'r') as f2:
-    keyword_list = json.load(f2)
-
 DISCORD_BOT_TOKEN = config["DISCORD_BOT_TOKEN"]
 
 class PreBot(discord.Client):
@@ -26,12 +23,34 @@ bot = PreBot()
 @bot.tree.command(name="add_keyword", description="Add a keyword to the pump.fun monitor list")
 @app_commands.describe(keyword="Coin name keyword you want to look for")
 async def add_keyword(interaction: discord.Interaction, keyword: str):
-    pass
+    with open('data/keywords.json', 'r') as f:
+        keyword_list = json.load(f)
+    
+    if keyword not in keyword_list:
+        keyword_list[keyword] = True
+        
+        with open('data/keywords.json', 'w') as f2:
+            json.dump(keyword_list, f2)
+        
+        await interaction.response.send_message(f"Success! Keyword '{keyword}' has been added.")
+    else:
+        await interaction.response.send_message(f"The keyword '{keyword}' already exists in the list.")
 
 @bot.tree.command(name="remove_keyword", description="Remove a keyword from the pump.fun monitor list")
 @app_commands.describe(keyword="Coin name keyword you want to remove")
 async def remove_keyword(interaction: discord.Interaction, keyword: str):
-    pass
+    with open('data/keywords.json', 'r') as f:
+        keyword_list = json.load(f)
+    
+    if keyword in keyword_list:
+        del keyword_list[keyword]
+        
+        with open('data/keywords.json', 'w') as f2:
+            json.dump(keyword_list, f2)
+        
+        await interaction.response.send_message(f"Success! Keyword '{keyword}' has been removed.")
+    else:
+        await interaction.response.send_message(f"The keyword '{keyword}' does not exist in the list.")
 
 @bot.tree.command(name="show_keyword", description="Show currently active keywords")
 async def remove_keyword(interaction: discord.Interaction):
@@ -42,7 +61,7 @@ async def self(interaction: discord.Interaction):
     embed = discord.Embed(title="Commands", description="All bot commands", color=discord.Colour.gold())
     embed.add_field(name=f"/add_keyword", value=f"Add a keyword to monitor for in pump.fun deploys")
     embed.add_field(name=f"/remove_keyword", value=f"Remove a keyword from pump.fun deploy monitoring")
-    embed.add_field(name=f"/show_keywords", value=f"Shows keywords being actively monitored (TO BE IMPLEMENTED))
+    embed.add_field(name=f"/show_keywords", value=f"Shows keywords being actively monitored (TO BE IMPLEMENTED)")
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
 if __name__ == '__main__':
