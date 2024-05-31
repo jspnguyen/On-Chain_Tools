e0020800c8e189add74e0f97ddfa26311fba61c5
jspnguyen <theantisine@gmail.com>
2024-05-31 12:07:29 -0700

    Added wallet stat checker

--- general_tools.py
+++ general_tools.py
@@ -1,10 +1,11 @@
-import discord, json
+import discord, json, requests
 from discord import app_commands
 
 with open('data/config.json', 'r') as f:
     config = json.load(f)
 
 DISCORD_BOT_TOKEN = config["DISCORD_BOT_TOKEN"]
+CIELO_API_KEY = config["CIELO_API_KEY"]
 
 class PreBot(discord.Client):
     def __init__(self, *args, **kwargs):
@@ -54,11 +55,45 @@ async def remove_keyword(interaction: discord.Interaction, keyword: str):
 
 @bot.tree.command(name="check_wallet", description="Display important stats for a wallet")
 @app_commands.describe(wallet="Sol or EVM wallet address")
-async def remove_keyword(interaction: discord.Interaction, wallet: str):
-    pass
+async def check_wallet(interaction: discord.Interaction, wallet: str):
+    url = f"https://feed-api.cielo.finance/api/v1/{wallet}/pnl/total-stats?chains=solana&cex_transfers=false"
+
+    headers = {
+        "accept": "application/json",
+        "X-API-KEY": CIELO_API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        wallet_data = response.json()
+        
+        if wallet_data["status"] == "ok":
+            realized_pnl_usd = wallet_data["realized_pnl_usd"]
+            realized_roi_percentage = wallet_data["realized_roi_percentage"]
+            tokens_traded = wallet_data["tokens_traded"]
+            unrealized_pnl_usd = wallet_data["unrealized_pnl_usd"]
+            unrealized_roi_percentage = wallet_data["unrealized_roi_percentage"]
+            winrate = wallet_data["winrate"]
+            average_holding_time = wallet_data["average_holding_time"]
+            combined_pnl_usd = wallet_data["combined_pnl_usd"]
+            combined_roi_percentage = wallet_data["combined_roi_percentage"]
+            
+            embed = discord.Embed(title=f"Report on {wallet}", color=discord.Colour.gold())
+            embed.add_field(name=f"", value=f"")
+            embed.add_field(name=f"", value=f"")
+            embed.add_field(name=f"", value=f"")
+            embed.add_field(name=f"", value=f"")
+            embed.add_field(name=f"", value=f"")
+            embed.add_field(name=f"", value=f"")
+            embed.add_field(name=f"", value=f"")
+            embed.add_field(name=f"", value=f"")
+            embed.add_field(name=f"", value=f"")
+            
+            await interaction.response.send_message(embed=embed, ephemeral=True)
 
 @bot.tree.command(name="show_keyword", description="Show currently active keywords")
-async def remove_keyword(interaction: discord.Interaction):
+async def show_keyword(interaction: discord.Interaction):
     pass
 
 @bot.tree.command(name="help", description="Shows commands for the bot") 
